dcae15679c2b1a320399e47dd0174446c9c489eb
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-03-31 12:19:38 +0500

    task2
    massivdagi elemetlar kiritilgan songa karralimi

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,7 +10,20 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.Write("Enter number N=> ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            random.Next(number);
+            Console.Write("Enter a diviser => ");
+            int diviser = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < number; i++)
+            {
+                if(i % diviser == 0)
+                {
+                    Console.WriteLine($"{i}, ");
+                }
+            }
         }
     }
 }
