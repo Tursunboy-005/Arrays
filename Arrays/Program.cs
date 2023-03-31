5d36fe3abaad08bf5818cb2cb1f032ccdc01c9fd
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-03-31 11:25:13 +0500

    array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,33 @@ namespace Arrays
 {
     internal class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            int input, butunson = int.MaxValue, maxCount = 0, count = 0, qiymat = 0;
+            do
+            {
+                Console.Write("butun son kiriting : ");
+                input = int.Parse(Console.ReadLine());
+
+                if (butunson == input)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        qiymat = butunson;
+                    }
+                    count = 1;
+                    butunson = input;
+                }
+            } while (count != 0);
+            Console.WriteLine("Bir birini takrorlaydigan sonlar uchun eng uzuni : ");
+
+
+
         }
     }
 }
